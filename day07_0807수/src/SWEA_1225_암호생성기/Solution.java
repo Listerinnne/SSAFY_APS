706391b4d4691086c4f86673a5e6c037180f870e
Huiju Kwon <00huiju00@gmail.com>
2024-08-07 17:55:28 +0900

    0807 과제

--- day07_0807수/src/SWEA_1225_암호생성기/Solution.java
+++ day07_0807수/src/SWEA_1225_암호생성기/Solution.java
@@ -1,10 +1,19 @@
 package SWEA_1225_암호생성기;
 
-import java.util.Queue;
+import java.util.Scanner;
 
 public class Solution {
-public static void main(String[] args) {
-
-Queue
-}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int t=0; t<10; t++) {	//테케 개수만큼 반복
+			System.out.printf("#%d ", sc.nextInt());
+			
+			
+			
+		}
+		
+	}
+	
 }
